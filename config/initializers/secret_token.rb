a65cc982f41f81883230f7525a1c9217d81d007c
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-10 10:50:45 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ppatch::Application.config.secret_key_base = '6ce51774f9ee33a476fa310363f3951f0f9f8c4bb3aff41962679b7b46bb94296243bb7e502715fe54338b23c243015b402a57f3818114326faf47829f0289d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ppatch::Application.config.session_store :cookie_store, key: '_ppatch_session'
