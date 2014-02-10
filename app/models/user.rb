e607a4a420dde080ccace51e3f1cd9d82e59c060
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-10 14:37:08 -0800

    config mock omniauth hash and added uid validation on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,22 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :uid, presence: true
+  validates :name, uniqueness: true
+  validates :email, uniqueness: true
   validates :phone, uniqueness: true
   validates :admin, inclusion: { in: [true, false] }
+
+
+
+  # def self.create_from_omniauth(auth_hash)
+  #   User.create!(
+  #     uid:        auth_hash[:uid],
+  #     avatar_url: auth_hash[:info][:image] || auth_hash[:info][:avatar],
+  #     name:       auth_hash[:info][:nickname],
+  #     secret:     auth_hash[:credentials][:secret], 
+  #     token:      auth_hash[:credentials][:token]
+  #   )
+  #   rescue ActiveRecord::RecordInvalid
+  #     nil
+  # end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,25 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-  
+
+  config.before(:suite) do
+    # Once you have enabled test mode, all requests 
+    # to OmniAuth will be short circuited
+    # to use the mock authentication hash. 
+    # A request to /auth/provider will redirect 
+    # immediately to /auth/provider/callback.
+
+    OmniAuth.config.test_mode = true
+
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+      :provider => 'twitter', 
+      :uid => '123545', 
+      info: { nickname: "Bookis", image: "some_url" },
+      credentials: {secret: "keepitsecret", token: "keepitsafe"}
+    })
+
+  end
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
