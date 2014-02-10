b0c7d8caa399bf500de3adc0eadba1bd2aefea96
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-10 15:54:00 -0800

    specs on omniauth session create

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,20 @@ class User < ActiveRecord::Base
   validates :phone, uniqueness: true
   validates :admin, inclusion: { in: [true, false] }
 
+  def self.find_or_create_from_omniauth(auth_hash)
+    User.find_by(uid: auth_hash[:uid]) || User.create_from_omniauth(auth_hash)
+  end
 
-
-  # def self.create_from_omniauth(auth_hash)
-  #   User.create!(
-  #     uid:        auth_hash[:uid],
-  #     avatar_url: auth_hash[:info][:image] || auth_hash[:info][:avatar],
-  #     name:       auth_hash[:info][:nickname],
-  #     secret:     auth_hash[:credentials][:secret], 
-  #     token:      auth_hash[:credentials][:token]
-  #   )
-  #   rescue ActiveRecord::RecordInvalid
-  #     nil
-  # end
+  def self.create_from_omniauth(auth_hash)
+    User.create!(
+      uid:        auth_hash[:uid],
+      avatar_url: auth_hash[:info][:image],
+      name:       auth_hash[:info][:nickname],
+      secret:     auth_hash[:credentials][:secret], 
+      token:      auth_hash[:credentials][:token]
+    )
+  rescue ActiveRecord::RecordInvalid
+    nil
+  end
 
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,14 +2,64 @@ require 'spec_helper'
 
 describe SessionsController do
   describe 'sign in process' do
-    
+    before(:each) do
+      request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
+    end
+
     context 'if user exists' do
+    let!(:user) { User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:twitter]) }
+      
       it 'assigns user to current user' do
+        get :create
+        expect(session[:user_id]).to eq assigns(:user).id
+      end
+
+      it "doesn't create another user" do
+        expect { get :create }.to_not change(User, :count).by(1)
+      end
+
+      it "redirects to home page" do
+        get :create
+        expect(response).to redirect_to root_path
       end
     end
 
     context 'if new user' do
+      it "creates a user" do
+        expect { get :create }.to change(User, :count).by(1)
+      end
     end
+
+
+      context "fails on twitter" do
+        before(:each) do 
+          request.env["omniauth.auth"] = {"uid" => nil, "info" => {} }
+        end
+
+        it "redirect to home with flash error" do
+          get :create
+          expect(response).to redirect_to root_path
+          expect(flash[:notice]).to eq "You do not exist!"
+        end
+      end
+
+      context "when failing to save the user" do
+        before { 
+          request.env["omniauth.auth"] = {:uid => "", 
+                                          :info => {name:"hello", image: "blah"}, 
+                                          :credentials => {secret:"", token: ""}
+                                          }
+        }
+        
+        it "redirect to home with flash error" do
+          create(:user, name:"UNIQ")
+          p User.last
+          get :create
+          p "should be 'hello", assigns(:user)
+          expect(response).to redirect_to root_path
+          expect(flash[:notice]).to eq "Failed to save the user"
+        end
+      end
 
   end
 
