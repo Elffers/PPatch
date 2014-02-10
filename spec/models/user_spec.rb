9e32877c03f5ad42244662518d2bc91d1390650d
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-10 11:39:39 -0800

    user validation specs initial

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,22 @@
 require 'spec_helper'
 
 describe User do
+  let(:user){ create(:user) }
   describe 'validations' do
-    
+    it 'is valid' do
+      expect(user).to be_valid
+    end
+
+    it 'must have a name' do
+      user.update(name: nil)
+      expect(user).to_not be_valid 
+    end
+
+    it 'must have unique name' do
+      user2 = build(:user, name: user.name)
+      user2.valid?
+      expect(user2.errors[:name]).to include 'has already been taken'
+    end
   end
   
 end
