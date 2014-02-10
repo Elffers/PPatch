00a44c7de6b37555676b524bcce18c7903f0a8b8
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-10 11:49:55 -0800

    user model validation specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+  validates :phone, uniqueness: true
+  validates :admin, inclusion: { in: [true, false] }
+
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,6 +29,17 @@ describe User do
       expect(user2.errors[:email]).to include 'has already been taken'
     end
 
+
+    it 'must have unique phone number' do
+      user2 = build(:user, phone: user.phone)
+      user2.valid?
+      expect(user2.errors[:phone]).to include 'has already been taken'
+    end
+
+    it 'must have admin status' do
+      user.update(admin: nil)
+      expect(user).to_not be_valid
+    end
   end
   
 end
