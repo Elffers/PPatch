aa1b8b896a319c64f3d31eb40b1916bab255404d
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-11 11:37:40 -0800

    events controller and routes initial commit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,52 @@
 class EventsController < ApplicationController
+  before_action :require_login, except: [:show, :index]
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
   def new
+    @event = Event.new
   end
 
+  def create
+    @event = Event.new(event_params)
+    user = User.find(session[:user_id])
+    if user.events << @event
+      flash[:notice] = "Event added!"
+      redirect_to event_path(@event)
+    else
+      flash[:notice] = "There was a problem saving your event."
+      render :new
+    end
+  end
+
+
   def index
+    @events = Event.all
   end
 
   def show
   end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private 
+
+  def event_params
+    params.require(:event).permit(:venue, :time, :description, :name, :user_id, :type)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def require_login
+    redirect_to sign_in_path, notice: "You must be signed in." if session[:user_id].nil?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,29 @@
 Ppatch::Application.routes.draw do
-  get "blog" => "posts#index", as: :posts
-  get "posts/show" => "posts#show", as: :post
+  
+  get "blog"          => "posts#index",       as: :posts
+  get "posts/show"    => "posts#show",        as: :post
   get "posts/new"
-  post "posts/new" => "posts#create", as: :create_post
+  post "posts/new"    => "posts#create",      as: :create_post
+  
   get "tools/new"
   get "tools/index"
   get "welcome/home"
-  get "events/new"
-  get "events/index"
-  get "events/show"
+  
+  get "events/new"    => "events#new",        as: :new_event
+  get "events"        => "events#index",      as: :events
+  post "/events"      => "events#create"
+
+  get "events/:id"         => "events#show",        as: :event
+  patch "/events/:id"      => "events#update"
+  delete "events/:id"      => "events#destroy"
+  get "events/:id/edit"    => "events#edit",        as: :edit_event
+
+
   get "users/show"
   get "users/new"
 
-  get "/signout" => "sessions#destroy", as: :sign_out
-  get "/signin" => "sessions#create", as: :sign_in
+  get "/signout"      => "sessions#destroy",  as: :sign_out
+  get "/signin"       => "sessions#create",   as: :sign_in
 
   get "/auth/twitter/callback", to: "sessions#create"
 
