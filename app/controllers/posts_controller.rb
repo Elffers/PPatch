ce286c49288ccc7e14f5ca561421c2d4c66dfe85
liz rush <liz@lizmrush.com>
2014-02-12 12:22:50 -0800

    updates before actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :current_user
-  before_action :require_login, only: [:new, :update, :destroy]
-  before_action :require_admin, only: [:new, :update, :destroy]
+  before_action :require_login, except: [:show, :index]
+  before_action :require_admin, except: [:show, :index]
   before_action :set_post, only: [:show, :update, :destroy, :edit]
 
 
