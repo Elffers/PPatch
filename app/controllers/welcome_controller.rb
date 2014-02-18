a76461ed2274270d18b7a3ed774c3ea17bc4a485
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-18 11:33:35 -0800

    specs for setting past events

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
+  before_action :past_events
+
   def home
     barometer = Barometer.new('Seattle')
     @weather = barometer.measure
@@ -6,4 +8,13 @@ class WelcomeController < ApplicationController
     @events_by_date = @events.group_by(&:date)
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
+
+  private
+  def past_events
+    @past_events = Event.all.keep_if {|event| event.date < Date.today}
+    p @past_events
+  end
+
+  # def upcoming_events
+  # end
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -9,4 +9,22 @@ describe WelcomeController do
     end
   end
 
+  describe "sets past events" do
+    let!(:past_event){ create(:event, date: Date.today - 1) }
+    let!(:future_event){ create(:event, date: Date.today + 1) }
+
+    it 'does not include future event' do
+      get :home
+      expect(assigns(:past_events)).to_not include future_event
+    end
+
+    it 'does include past event' do
+      get :home
+      expect(assigns(:past_events)).to include past_event
+    end
+  end
+
+  describe "sets upcoming events" do
+  end
+
 end
