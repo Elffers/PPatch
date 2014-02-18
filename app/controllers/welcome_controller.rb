be8a46a3e51ac937e80a237023a07f28a3e10257
liz rush <liz@lizmrush.com>
2014-02-18 14:24:39 -0800

    moves weather to application controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,5 @@
 class WelcomeController < ApplicationController
   def home
-    barometer = Barometer.new('Seattle')
-    @weather = barometer.measure
     @events = Event.all
     @events_by_date = @events.group_by(&:date)
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
