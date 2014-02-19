14f386c2fa2e78e251eb118e370e600360361044
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-18 17:05:35 -0800

    add comment

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,12 +1,12 @@
 class PostMailer < ActionMailer::Base
   before_action :set_recipients
+
   default from: "admin@thewormhole.com",
           to: Proc.new { @recipients.pluck(:email) }
 
   def new_post(post_id, user_id)
-    p @recipients
     @post = Post.find(post_id)
-    # @user = User.find(user_id)
+    # @user = User.find(user_id) # problem with @user being available in view
     mail( 
         # to: "#{@user.email}", 
         subject: "The Worm Hole has updated its Blahg."
