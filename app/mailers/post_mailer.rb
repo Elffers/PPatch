e8eb786b4149a93ae694be8bd484c37a66e33f2c
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-18 17:01:27 -0800

    email sends to multiple recipients

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,12 +1,21 @@
 class PostMailer < ActionMailer::Base
-  default from: "admin@thewormhole.com"
+  before_action :set_recipients
+  default from: "admin@thewormhole.com",
+          to: Proc.new { @recipients.pluck(:email) }
 
   def new_post(post_id, user_id)
+    p @recipients
     @post = Post.find(post_id)
-    @user = User.find(user_id)
+    # @user = User.find(user_id)
     mail( 
-        to: "#{@user.email}", 
+        # to: "#{@user.email}", 
         subject: "The Worm Hole has updated its Blahg."
     )
+  end
+
+  private
+
+  def set_recipients
+    @recipients = User.where(preferences: true)
   end
 end
--- spec/mailers/post_mailer_spec.rb
+++ spec/mailers/post_mailer_spec.rb
@@ -1,9 +1,10 @@
 require "spec_helper"
 
 describe PostMailer do
-  let(:user){ create(:user) }
-  let(:post){ create(:post) }
-  let(:mail) { PostMailer.new_post(post.id, user.id) }
+  let!(:user){ create(:user) }
+  let!(:unsubscribed_user){create(:user, preferences: false)}
+  let!(:post){ create(:post) }
+  let!(:mail) { PostMailer.new_post(post.id, user.id) }
 
   it 'sends mail to user email' do
     expect(mail.to).to include user.email
@@ -16,4 +17,9 @@ describe PostMailer do
   it 'sends sets subject' do
     expect(mail.subject).to eq "The Worm Hole has updated its Blahg."
   end 
+
+  it 'sends mail to user who wants it' do
+    p mail.inspect
+    expect(mail.to).to include user.email
+  end
 end
