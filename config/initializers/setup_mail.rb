c2affefdbde82b7b87a6c5a1ab8b19ce228b0e9c
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-20 11:26:36 -0800

    change domain name

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,12 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.mandrillapp.com",
   :port                 => 587,
-  :domain               => "localhost:3000",
+  :domain               => "thewormhole.herokuapp.com",
   :user_name            => "admin@wormhole.com",
   :password             => ENV['MANDRILL_KEY'],
   :authentication       => "login",
   :enable_starttls_auto => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.default_url_options[:host] = "thewormhole.herokuapp.com"
 # Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
