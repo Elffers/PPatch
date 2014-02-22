120b1c1b9c9ec48efaa5f7321c145e452710974b
liz rush <liz@lizmrush.com>
2014-02-22 11:49:07 -0800

    tools displayed in table, ajax not appending

--- app/assets/javascripts/addtool.js
+++ app/assets/javascripts/addtool.js
@@ -16,16 +16,16 @@ $(function() {
 $(document).ready(function() {
   $(".tool-action").click(function() {
 
-    var items = $(".tools");
+    var tools = $(".tools");
 
     $.ajax({
       url: $(this).parents('#tool-form').attr("action"),
       type: 'POST',
       dataType: 'json',
-      data: {item: {name: $("#tool_name").val() }},
+      data: {tool: {name: $("#tool_name").val() }},
       success: function(data, textStatus, xhr) {
         var delete_button = '<a class="delete" data-method="delete" href="/tools/'+ data.list_id +'/tools/' + data.id + '" rel="nofollow">delete tool</a>'
-        items.append("<tr><td>"+ data.name + "</td><td>" + delete_button + "</td></tr>");
+        tools.append("<tr><td>"+ data.name + "</td><td>" + delete_button + "</td></tr>");
       },
       error: function(xhr, textStatus, errorThrown) {
         alert("There was a problem adding this tool");
