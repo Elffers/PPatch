aa24af0e7dc18ee14a8990cef8f1bcb1cddbe535
liz rush <liz@lizmrush.com>
2014-02-22 11:42:16 -0800

    tool index page with JS to show add tool area

---
+++ app/assets/javascripts/addtool.js
@@ -0,0 +1,36 @@
+$(function() {
+    $('#add-tool-area').hide();
+
+  $(".add-tool-link").click(function(e){
+    $('#add-tool-link').hide();
+    $('#add-tool-area').show();
+    e.preventDefault();
+  });
+
+  $(".tool-action").click(function(e){
+    $("#add-tool-area").hide();
+  });
+});
+
+
+$(document).ready(function() {
+  $(".tool-action").click(function() {
+
+    var items = $(".tools");
+
+    $.ajax({
+      url: $(this).parents('#tool-form').attr("action"),
+      type: 'POST',
+      dataType: 'json',
+      data: {item: {name: $("#tool_name").val() }},
+      success: function(data, textStatus, xhr) {
+        var delete_button = '<a class="delete" data-method="delete" href="/tools/'+ data.list_id +'/tools/' + data.id + '" rel="nofollow">delete tool</a>'
+        items.append("<tr><td>"+ data.name + "</td><td>" + delete_button + "</td></tr>");
+      },
+      error: function(xhr, textStatus, errorThrown) {
+        alert("There was a problem adding this tool");
+      }
+    });
+    return false;
+  });
+});
